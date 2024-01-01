6e7fa6727a2fc6a1a99566f8831899d4516136a7
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-01 22:03:17 +0000

    create play method in rock-paper-scissors.js file and implement game logic

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,8 +1,36 @@
 let answerArray = ["ROCK", "PAPER", "SCISSORS"];
-let randomNumber = Math.floor(Math.random() * 3);
 
 function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
     return answerArray[randomNumber];
 }
 
-console.log(getComputerChoice());
+function play(playerChoice, computerChoice){
+    console.log(playerChoice);
+    console.log(computerChoice);
+    
+    if(playerChoice=='ROCK' && computerChoice=='SCISSORS'){
+        return 'YOU WIN! ROCK BEATS SCISSORS'
+    }else if(computerChoice=='ROCK' && playerChoice=='SCISSORS'){
+        return 'YOU LOSE! ROCK BEATS SCISSORS'
+    }
+
+    if(playerChoice=='PAPER' && computerChoice=='ROCK'){
+        return 'YOU WIN! PAPER BEATS ROCK'
+    }else if(computerChoice=='PAPER' && playerChoice=='ROCK'){
+        return 'YOU LOSE! PAPER BEATS ROCK'
+    }
+
+    if(playerChoice=='SCISSORS' && computerChoice=='PAPER'){
+        return 'YOU WIN! SCISSORS BEATS PAPER'
+    }else if(computerChoice=='SCISSORS' && playerChoice=='PAPER'){
+        return 'YOU LOSE! SCISSORS BEATS PAPER'
+    }
+
+    if(playerChoice==computerChoice){
+        return 'DRAW!';
+    }
+
+}
+
+console.log(play(getComputerChoice(),getComputerChoice()));
