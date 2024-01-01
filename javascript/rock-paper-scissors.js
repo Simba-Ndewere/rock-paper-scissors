bd4e450a002bd38c064aae6eecf5e1e360d17e44
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-01 22:35:50 +0000

    create game method and implement logic that allows a game of 5 and keeps track of scores

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,36 +1,49 @@
 let answerArray = ["ROCK", "PAPER", "SCISSORS"];
+let gameScore = [0,0];
+
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     return answerArray[randomNumber];
 }
 
-function play(playerChoice, computerChoice){
-    console.log(playerChoice);
-    console.log(computerChoice);
-    
+function playSingeRound(playerChoice, computerChoice){
+
     if(playerChoice=='ROCK' && computerChoice=='SCISSORS'){
+        gameScore[0] = gameScore[0] + 1;
         return 'YOU WIN! ROCK BEATS SCISSORS'
     }else if(computerChoice=='ROCK' && playerChoice=='SCISSORS'){
+        gameScore[1] = gameScore[1] + 1;
         return 'YOU LOSE! ROCK BEATS SCISSORS'
     }
 
     if(playerChoice=='PAPER' && computerChoice=='ROCK'){
+        gameScore[0] = gameScore[0] + 1;
         return 'YOU WIN! PAPER BEATS ROCK'
     }else if(computerChoice=='PAPER' && playerChoice=='ROCK'){
+        gameScore[1] = gameScore[1] + 1;
         return 'YOU LOSE! PAPER BEATS ROCK'
     }
 
     if(playerChoice=='SCISSORS' && computerChoice=='PAPER'){
+        gameScore[0] = gameScore[0] + 1;
         return 'YOU WIN! SCISSORS BEATS PAPER'
     }else if(computerChoice=='SCISSORS' && playerChoice=='PAPER'){
+        gameScore[1] = gameScore[1] + 1;
         return 'YOU LOSE! SCISSORS BEATS PAPER'
     }
 
     if(playerChoice==computerChoice){
         return 'DRAW!';
     }
-
 }
 
-console.log(play(getComputerChoice(),getComputerChoice()));
+function game(){
+    for(let a = 0; a < 5; a++){
+        let result = playSingeRound(getComputerChoice(),getComputerChoice());
+        console.log(result);
+    }
+}
+
+console.log(game());
+console.log(gameScore);
