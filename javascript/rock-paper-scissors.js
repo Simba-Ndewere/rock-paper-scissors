7d3eecbc19518a4b54be321c17ae4981de9c4cc2
Simba Ndewere <ndeweresimba@gmail.com>
2024-01-05 03:23:59 +0000

    create game round result hidden div

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,5 +1,6 @@
 let answerArray = ["ROCK", "PAPER", "SCISSORS"];
 let gameScore = [0,0];
+let handsDiv = document.getElementById("hands");
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
@@ -57,6 +58,85 @@ function winner(){
     }
 }
 
-console.log(game());
-console.log(gameScore);
-console.log(winner());
+function imageClicked(choice){
+    handsDiv.style.display = "none";
+    switch(choice){
+        case 1: console.log("rock played");
+        break;
+        case 2: console.log("paper played");
+        break;
+        case 3: console.log("scissors played");
+        break;
+    }
+    showComputerChoice(choice);
+}
+
+function showComputerChoice(userChoice){
+    const game = document.querySelector("#game");
+
+    const results = document.createElement("div");
+    results.classList.add("results");
+
+    const resultsTop= document.createElement("div");
+    resultsTop.classList.add("resultsTop");
+
+    const handsShow = document.createElement("div");
+    handsShow.classList.add("handsShow");
+
+    const youPicked = document.createElement("div");
+    youPicked.textContent = "YOU";
+
+    const housePicked = document.createElement("div");
+    housePicked.textContent = "COMPUTER";
+
+    const firstPick = document.createElement("div");
+    const firstPickImage = document.createElement('img');    
+
+    const result = document.createElement("div");
+    result.textContent = 'YOU LOSE';
+    result.classList.add("result");
+
+    const secondPick = document.createElement("div");
+    const secondPickImage = document.createElement('img'); 
+
+    game.appendChild(results);
+
+    results.appendChild(resultsTop);
+    results.appendChild(handsShow);
+
+    resultsTop.appendChild(youPicked);
+    resultsTop.appendChild(housePicked);
+
+    handsShow.appendChild(firstPick);
+    firstPick.appendChild(firstPickImage);
+
+    handsShow.appendChild(result);
+
+    handsShow.appendChild(secondPick);
+    secondPick.appendChild(secondPickImage);
+
+    secondPickImage.src = "icons/rock.jpeg";
+
+   // var imgElement = document.createElement('img');
+   // const div = document.createElement("div");
+   // const hands = document.querySelector("#game");
+   // div.classList.add("results");
+    
+   // const youPicked = document.createElement();
+
+
+    switch(userChoice){
+        case 1: firstPickImage.src = "icons/rock.jpeg";
+        break;
+        case 2: firstPickImage.src = "icons/paper.jpeg";
+        break;
+        case 3: firstPickImage.src = "icons/scissors.jpeg";
+        break;
+    }
+   
+
+   // imgElement.width = 300;
+   // imgElement.height = 300;
+    //div.appendChild(imgElement);
+    //hands.appendChild(div);
+}
